20ab6e546f4a67c6a82bbd109a883fe707787914
DavinaRJ116 <davinarj550@gmail.com>
2025-12-02 12:34:05 +0530

    changes made

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("welcome to the git course");
+console.log("welcome to the git playlist");
+for (var i = 0; i < 5; i++) {
+  console.log("this is line number " + i);
+}
+console.log("end of the git playlist");
